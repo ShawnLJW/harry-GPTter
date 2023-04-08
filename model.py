ff00caa311a571b4a0e67ae47c6f01efe0445f0e
Shawn Lee <thisisshawnlee@gmail.com>
2023-04-08 10:43:13 +0800

    Update model.py

--- model.py
+++ model.py
@@ -1,106 +1,99 @@
-from dataclasses import dataclass
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from dataclasses import dataclass
+
 
 @dataclass
 class ModelConfig:
-    block_size: int = 1024
-    vocab_size: int = 4096
-    n_layer: int = 12
-    n_head: int = 12
-    n_embd: int = 512
-    dropout: float = 0.3
+    block_size: int = 256
+    vocab_size: int = 65
+    n_layer: int = 8
+    n_head: int = 8
+    n_embd: int = 384
+    dropout: float = 0.2
     bias: bool = False
 
-class AttentionHead(nn.Module):
+
+config = ModelConfig()
+
+
+class TransformerBlock(nn.Module):
     def __init__(self, config):
         super().__init__()
-        self.key = nn.Linear(config.n_embd, config.head_size, bias=False)
-        self.query = nn.Linear(config.n_embd, config.head_size, bias=False)
-        self.value = nn.Linear(config.n_embd, config.head_size, bias=False)
-        self.dropout = nn.Dropout(config.dropout)
 
-    def forward(self, x):
-        keys = self.key(x)
-        queries = self.query(x)
-        values = self.value(x)
-        
-        y = F.scaled_dot_product_attention(
-            queries, keys, values,
-            attn_mask=None, dropout_p=self.dropout, is_causal=True
-        )
-        
-        return y
+        self.linear = nn.Linear(
+            config.n_embd, config.n_embd*3, bias=config.bias)
+        self.attention = nn.MultiheadAttention(
+            config.n_embd, config.n_head, config.dropout, config.bias, batch_first=True)
+        mask = torch.triu(torch.ones(config.block_size,
+                          config.block_size), diagonal=1)
+        mask = mask.masked_fill(mask == 1, float('-inf'))
+        self.register_buffer('mask', mask)
 
-class MultiHeadedAttention(nn.Module):
-    def __init__(self, config):
-        super().__init__()
-        self.heads = nn.ModuleList([AttentionHead(config.head_size) for _ in range(config.n_head)])
-        self.projection = nn.Linear(config.n_embd, config.n_embd)
-        self.dropout = nn.Dropout(config.dropout)
-
-    def forward(self, x):
-        x = torch.cat([model(x) for model in self.heads], dim=-1)
-        x = self.projection(x)
-        x = self.dropout(x)
-        return x
-
-class Block(nn.Module):
-    def __init__(self, config):
-        super().__init__()
-        self.self_attention = MultiHeadedAttention(config.n_head , config.n_embd//config.n_head)
-        self.feedforward = nn.Sequential(
-            nn.LayerNorm(config.n_embd),
-            nn.Linear(config.n_embd, 4*config.n_embd),
-            nn.ReLU(),
-            nn.Linear(4*config.n_embd, config.n_embd),
-            nn.LayerNorm(config.n_embd),
+        self.layer_norm = nn.LayerNorm(config.n_embd)
+        self.mlp = nn.Sequential(
+            nn.Linear(config.n_embd, 4 * config.n_embd, bias=config.bias),
+            nn.GELU(),
+            nn.Linear(4 * config.n_embd, config.n_embd, bias=config.bias),
             nn.Dropout(config.dropout)
         )
 
     def forward(self, x):
-        x = x + self.self_attention(x)
-        x = x + self.feedforward(x)
+        b, t, c = x.shape
+        query, key, value = self.linear(x).split(config.n_embd, dim=2)
+        x = x + self.attention(query, key, value,
+                               need_weights=False, attn_mask=self.mask[:t, :t])[0]
+        x = self.layer_norm(x)
+        x = x + self.mlp(x)
+        x = self.layer_norm(x)
         return x
 
-class AttentionModel(nn.Module):
+
+class GenerativeTransformer(nn.Module):
     def __init__(self, config):
         super().__init__()
-        self.config = config
-        
-        self.token_embedding = nn.Embedding(config.vocab_size, config.n_embd)
-        self.position_embedding = nn.Embedding(config.block_size, config.n_embd)
+
+        self.block_size = config.block_size
+
+        self.text_embed = nn.Embedding(config.vocab_size, config.n_embd)
+        self.pos_enc = nn.Embedding(config.block_size, config.n_embd)
+        self.dropout = nn.Dropout(config.dropout)
         self.blocks = nn.Sequential(
-            *[Block(config) for _ in range(config.n_layer)]
-        )
-        self.lm = nn.Linear(config.n_embd, config.vocab_size)
+            *[TransformerBlock(config) for _ in range(config.n_layer)])
+        self.linear = nn.Linear(
+            config.n_embd, config.vocab_size, bias=config.bias)
 
-    def forward(self, tokens, targets=None):
-        num_blocks, num_tokens = tokens.shape
-        pos = torch.arange(0,num_tokens,dtype=torch.long,device=tokens.device)
+    def get_num_params(self):
+        return sum(p.numel() for p in self.parameters())
 
-        token_embd = self.token_embedding(tokens)
-        position_embd = self.position_embedding(pos)
-        embedding = token_embd + position_embd
-        logits = self.blocks(embedding)
-        logits = self.lm(logits)
+    def forward(self, x, targets=None):
+        device = x.device
+        b, t = x.shape
+        assert t <= self.block_size, f"Cannot forward sequence of length {t}, block size is only {self.block_size}"
 
-        if targets == None:
-            return logits, None
+        pos = torch.arange(0, t, dtype=torch.long, device=device).unsqueeze(0)
+        x = self.text_embed(x) + self.pos_enc(pos)
+        x = self.dropout(x)
+        x = self.blocks(x)
 
-        num_blocks, num_tokens, num_chars = logits.shape
-        logits = logits.view(num_blocks * num_tokens, num_chars)
-        targets = targets.view(num_blocks * num_tokens)
-        loss = F.cross_entropy(logits, targets)
-        return logits, loss
+        if targets is None:
+            x = self.linear(x[:, -1, :])
+            x = F.softmax(x, dim=-1)
+            loss = None
+        else:
+            x = self.linear(x)
+            loss = F.cross_entropy(
+                x.view(-1, x.size(-1)), targets.view(-1), ignore_index=-1)
 
-    def generate(self, tokens, length):
-        for _ in range(length):
-            logits, _ = self(tokens[:,-self.config.block_size:])
-            logits = logits[:,-1,:]
-            probs = F.softmax(logits, dim=-1)
-            sample = torch.multinomial(probs, 1)
-            tokens = torch.cat((tokens, sample), dim=1)
-        return tokens
+        return x, loss
+
+    @torch.no_grad()
+    def generate(self, idx, max_tokens):
+        for _ in range(max_tokens):
+            idx_cond = idx if idx.size(
+                1) <= self.block_size else idx[:, -self.block_size:]
+            new_token, _ = self(idx_cond)
+            new_token = torch.multinomial(new_token, num_samples=1)
+            idx = torch.cat((idx, new_token), dim=1)
+        return idx
